9e0d8471f768d6f3033c6bd206e57411aab3f1ef
hasan-at <hassn.atli.1@gmail.com>
2020-08-18 12:29:01 +0200

    Stayl endern

--- main.js
+++ main.js
@@ -68,3 +68,15 @@ var todolist = new Vue({
         }
     }
 });
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
+}
